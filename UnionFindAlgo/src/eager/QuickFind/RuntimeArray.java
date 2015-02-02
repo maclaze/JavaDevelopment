fbed89aa886bd62cb2de2fb3bdef81107cfb96ca
nars <nars@groupon.com>
2015-02-02 15:55:38 +0530

    added simple union find

--- UnionFindAlgo/src/eager/QuickFind/RuntimeArray.java
+++ UnionFindAlgo/src/eager/QuickFind/RuntimeArray.java
@@ -6,12 +6,12 @@ import java.util.Scanner;
 public class RuntimeArray {
 
 	private static int[] inputArray;
+	private static Scanner scan;
 
 	public static int readInput(){
-		Scanner scan = new Scanner(System.in);
-		System.out.println("enter");
+		scan = new Scanner(System.in);
+		System.out.println("\n enter a value");
 		int readIntVal = scan.nextInt();
-		scan.close();
 		return readIntVal;
 
 	}
@@ -24,10 +24,42 @@ public class RuntimeArray {
 			System.out.println("invlaid integer");
 		}
 		int[] inputAr = new int[n];
-		System.out.println(inputArray.length + ": is the array size");
+		for (int i = 0; i < inputAr.length; i++) {
+			inputAr[i] = i;
+		}
 		return inputAr;
 	}
+
 	public static void main(String[] args) {
-		inputArray = initializeInput();;
+		inputArray = initializeInput();
+		boolean isConnected = false;
+		do {
+			int p = readInput();
+			int q = readInput();
+			isConnected = find(p,q);
+			System.out.println(isConnected);
+			if (!isConnected) {
+				union(p, q);
+			}
+		}while(!isConnected);
+			scan.close();
+		for (int i = 0; i < inputArray.length; i++) {
+			System.out.println(inputArray[i]);
+		}
 	}
+
+	public static boolean find(int p, int q)
+	{
+		return inputArray[p] == inputArray[q];
+	}
+
+	public static void union(int p, int q)
+	{
+		int pid = inputArray[p];
+		for (int i = 0; i < inputArray.length; i++) {
+			if(inputArray[i] == pid)
+				inputArray[i] = inputArray[q];
+		}
+	}
+
 }
